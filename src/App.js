c2b118565618e3a5a026f87afeaac52515c15b94
konstapascal <pascalkonstantinos@gmail.com>
2020-08-06 20:58:21 +0200

    added basic website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+	return (
+		<div className='App'>
+			<h2>COVID-19 tracker</h2>
+			<Button variant='primary'>Primary</Button>{' '}
+		</div>
+	);
+}
+
+export default App;
